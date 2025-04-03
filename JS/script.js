4e38d58668d126df79e8fcfe715093a1f7346326
Dezectorkillone <Lehsilva715@gmail.com>
2025-04-03 20:38:09 -0300

    att2

--- JS/script.js
+++ JS/script.js
@@ -14,19 +14,19 @@ function calcular(){
     arroz1 = Number(document.getElementById("arroz1").value)
     macarrao = Number(document.getElementById("macarrao").value)
     equipe = document.getElementById("equipe").value
-     soma = (mascote) + (homenagem) + (2 *(leite)) + (30 * (kit)) +(oleo) + (15 *(suplemento)) + (2 * (feijao2)) + (feijao1) + (5 *(arroz5)) +(arroz1) + (0.5 *(macarrao))
+     soma = (mascote) + (homenagem) + (2 *(leite)) + (kit)+(oleo) + (suplemento) + (2 * (feijao2)) + (feijao1) + (5 *(arroz5)) +(arroz1) + (0.5 *(macarrao))
     if(equipe == "Laranja"){
-            if(kit>= 97){
-             soma = soma + 5000 + (( kit - 97 ) * 30)
+            if(kit>= 97 && suplemento >= 49){
+             soma = soma + 5000 + (( kit - 97 ) * 30) + ((suplemento - 49)*15)
             }
-            else if (kit>= 78){
-                soma = soma + 4000 + (( kit - 78) * 30)
+            else if (kit>= 78 && suplemento >= 39){
+                soma = soma + 4000 + (( kit - 78) * 30)+ ((suplemento - 39)*15)
             }
-            else if (kit>= 49){
-                soma = soma + 2500 + (( kit - 49) * 30)
+            else if (kit>= 49 && suplemento >= 25){
+                soma = soma + 2500 + (( kit - 49) * 30)+ ((suplemento - 25)*15)
             }
-            else if (kit>= 19){
-                soma = soma + 1000 + ((kit - 19) * 30)
+            else if (kit>= 19 && suplemento >= 10){
+                soma = soma + 1000 + ((kit - 19) * 30) + ((suplemento - 10)*15)
         }
     }
    
